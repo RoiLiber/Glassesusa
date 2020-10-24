875011682a814412b6da56d37703854458deab04
unknown <liber.roi@gmail.com>
2020-10-24 06:53:57 +0300

    Updates

---
+++ precache-manifest.04162e98b60f2c06f2beec0c9248a74a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d23bddd8f32171086137166a246bb3a",
+    "url": "/Glassesusa/index.html"
+  },
+  {
+    "revision": "97e52b6d934360f06f13",
+    "url": "/Glassesusa/static/css/main.c987ad46.chunk.css"
+  },
+  {
+    "revision": "76de9602f6fd2a4d6674",
+    "url": "/Glassesusa/static/js/2.e3828fa4.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/Glassesusa/static/js/2.e3828fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e52b6d934360f06f13",
+    "url": "/Glassesusa/static/js/main.a4435b44.chunk.js"
+  },
+  {
+    "revision": "debb157d6110be14d09f",
+    "url": "/Glassesusa/static/js/runtime-main.3465a2c7.js"
+  },
+  {
+    "revision": "bd597fcdbb6a286a5d8040a9bd29ea25",
+    "url": "/Glassesusa/static/media/logo.bd597fcd.png"
+  },
+  {
+    "revision": "bdb7eddb249df64394854c8820633158",
+    "url": "/Glassesusa/static/media/logo_dark.bdb7eddb.png"
+  }
+]);
