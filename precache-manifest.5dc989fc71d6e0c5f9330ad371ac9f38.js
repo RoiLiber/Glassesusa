f4cb7f377e1c86d1c35fda3f9609e65dc7a2769f
unknown <liber.roi@gmail.com>
2020-10-24 03:36:58 +0300

    Updates

---
+++ precache-manifest.5dc989fc71d6e0c5f9330ad371ac9f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8327e30f2f85f68006eb72cf00a5eaeb",
+    "url": "/Glassesusa/index.html"
+  },
+  {
+    "revision": "d380e054520fd3c05868",
+    "url": "/Glassesusa/static/css/main.ac75af9e.chunk.css"
+  },
+  {
+    "revision": "d3c5f8b32b9639429530",
+    "url": "/Glassesusa/static/js/2.7890b821.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/Glassesusa/static/js/2.7890b821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d380e054520fd3c05868",
+    "url": "/Glassesusa/static/js/main.e571e757.chunk.js"
+  },
+  {
+    "revision": "debb157d6110be14d09f",
+    "url": "/Glassesusa/static/js/runtime-main.3465a2c7.js"
+  },
+  {
+    "revision": "bd597fcdbb6a286a5d8040a9bd29ea25",
+    "url": "/Glassesusa/static/media/logo.bd597fcd.png"
+  }
+]);
